e6abeae65c253fa89c613f6b066ca77aabd744e1
ParkGeunHyuk95 <rmsgur87@genmatefield.com>
2023-08-15 18:39:37 +0900

    FEAT: add circle to ground, apply mesh reflectormaterial at ground

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -1,13 +1,22 @@
+import { MeshReflectorMaterial } from '@react-three/drei';
 import { useLoader } from '@react-three/fiber';
 import React, { useEffect } from 'react';
 import { TextureLoader } from 'three';
 
 const Ground = () => {
     const gridMap = useLoader(TextureLoader, process.env.PUBLIC_URL + '/textures/grid.png');
+    const aoMap = useLoader(TextureLoader, process.env.PUBLIC_URL + '/textures/ground-ao.png');
     useEffect(() => {
         gridMap.anisotropy = 16;
     }, [gridMap]);
-    return <div></div>;
+    return (
+        <>
+            <mesh position={[-2.285, -0.015, -1.325]} rotation-x={-Math.PI * 0.5} rotation-z={-0.079}>
+                <circleGeometry args={[6.12, 50]} />
+                <MeshReflectorMaterial />
+            </mesh>
+        </>
+    );
 };
 
 export default Ground;
--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,5 +1,6 @@
 import { Environment, OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import React, { Suspense } from 'react';
+import Ground from './Ground';
 import Track from './Track';
 
 export function Scene() {
@@ -9,6 +10,7 @@ export function Scene() {
             <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} />
             <OrbitControls target={[-2.64, -0.71, 0.03]} />
             <Track />
+            <Ground />
         </Suspense>
     );
 }
